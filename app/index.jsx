7f5b465faf1b5ebd7afa5d918b95c70c6de2da07
“janjanmalakas <johnrochelabella1@gmail.com>
2025-08-22 16:57:59 +0800

    color was changed

--- app/index.jsx
+++ app/index.jsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   link: {
     marginVertical: 20,
     padding: 16,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#fcf803ff',
     color: 'white',
     borderRadius: 8,
   },
